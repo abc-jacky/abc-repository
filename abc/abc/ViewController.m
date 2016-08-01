755323af377369e6c7142f240f9bbc035a3cfa77
lanou <sobody@126.com>
2016-08-01 11:12:35 +0800

    初始化工程

---
+++ abc/abc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  abc
+//
+//  Created by lanou on 16/8/1.
+//  Copyright © 2016年 JSP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"nihaoshuaia！！");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
